31128daea7aa23de445a9ff4611c64ae460aff9f
Sethea <sethea.sunly@gmail.com>
2025-06-09 18:05:40 +1000

    debug

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '../../../../generated/prisma';
+import { PrismaClient } from '@/generated/prisma';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '@/utils/jwt';
 
